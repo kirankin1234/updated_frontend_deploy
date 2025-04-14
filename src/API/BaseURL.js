b59f44adc980183ab765b48af711bd72a07df5c2
Kiran Gaikwad <kiran899964@gmail.com>
2025-04-14 18:37:12 +0530

    url

--- src/API/BaseURL.js
+++ src/API/BaseURL.js
@@ -1,2 +1,2 @@
 // src/API/BaseURL.js
-export const BASE_URL = "http://localhost:5001"; // Change this for deployment
+export const BASE_URL = "https://updated-backend-deploy.onrender.com"; // Change this for deployment
